aa16594941cc45992ae745a6d5de1470ee0dd03e
Benjamin Kuzava <benjaminkuzava@marshas-air.gateway.sonic.net>
2021-07-02 10:09:34 -0700

    basic routes and lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
+import { Suspense, lazy } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+
+const Home = lazy(() => import("./pages/Home"));
+const BrowseLevels = lazy(() => import("./pages/BrowseLevels"));
+const CreateLevel = lazy(() => import("./pages/CreateLevel"));
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the App</h1>
-    </div>
+    <Router>
+      <Suspense fallback={<p>... Loading</p>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/browse" component={BrowseLevels} />
+          <Route exact path="/create" component={CreateLevel} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ client/src/pages/BrowseLevels.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BrowseLevels() {
+  return (
+    <div>
+      <h1>Browse Levels</h1>
+    </div>
+  );
+}
+
+export default BrowseLevels;
